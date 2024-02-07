d3b2f830afd2bced091a4359e1d5292f170e29fa
Alessandro Anelli <127589937+APA2306@users.noreply.github.com>
2024-02-07 10:43:30 +0100

    Add files via upload

---
+++ Rubrica-main/Anelli.Alessandro.4I.rubricaUnoAMolti/MainWindow.xaml.cs
@@ -0,0 +1,82 @@
+﻿using Anelli.Alessandro._4I.rubricaUnoAMolti.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Anelli.Alessandro._4I.rubricaUnoAMolti
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<Contatto> contatti = new List<Contatto>();
+        List<Persona> persone = new List<Persona>();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            try 
+            { 
+                StreamReader fcontatti = new StreamReader("Data/Contatti.csv");
+                fcontatti.ReadLine();
+                while(!fcontatti.EndOfStream) 
+                {
+                    string rigac = fcontatti.ReadLine();
+                    Contatto c = new Contatto(rigac);
+                    contatti.Add(c);
+                }
+
+                //dataGridContatti.ItemsSource = contatti;
+
+                StreamReader fpersone = new StreamReader("Data/Persone.csv");
+                fpersone.ReadLine();
+                while (!fpersone.EndOfStream)
+                {
+                    string rigap = fpersone.ReadLine();
+                    Persona c = new Persona(rigap);
+                    persone.Add(c);
+                }
+
+                dataGridPersone.ItemsSource = persone;
+
+            }
+            catch  
+            {
+                MessageBox.Show("Possibile problema nei file .csv");
+                Environment.Exit(0);
+            }
+        }
+
+        private void dataGridPersone_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Persona p = e.AddedItems[0] as Persona;
+
+            if( p != null ) 
+            {
+                statusBar.Text = $"hai selezionato {p.Nome} {p.Cognome}";
+
+                List<Contatto> contattifiltrati = new List<Contatto>();
+                foreach (var item in contatti)
+                    if(item.IdPersona == p.IdPersona)
+                        contattifiltrati.Add(item);
+                dataGridContatti.ItemsSource = contattifiltrati;
+            }
+        }
+    }
+    
+}
---
+++ Rubrica-main/Anelli.Alessandro.4I.rubricaUnoAMolti/Models/Contatto.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Anelli.Alessandro._4I.rubricaUnoAMolti.Models
+{
+    public enum TipoContatto { nessuno, Email, Telefono, Web, Instragram }
+    public class Contatto
+    {
+        public int  IdPersona { get; set; }
+        public TipoContatto Tipo { get; set; }
+        public string Valore { get; set; }
+
+        public Contatto(string r)
+        {
+            string[] values = r.Split(';');
+            if (values.Count() != 3)
+                throw new Exception("Le righe del file contatti .csv devono essere di tre colonne");
+
+            if (int.TryParse(values[0], out int id) && Enum.TryParse(values[1], out TipoContatto c))
+                {
+                    this.IdPersona = id;
+                    this.Tipo = c;
+                    this.Valore = values[2];
+                }
+        }
+    }
+}
